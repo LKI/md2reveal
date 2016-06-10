65dd9215de27adb4b76b4ba0e8350f8dfc0ee612
Lirian Su <liriansu@gmail.com>
2016-06-10 06:14:02 +0000

    Add MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os.path
 
 setup(
     name='md2reveal',
-    version='1.0.1',
+    version='1.0.2',
     author='Lirian Su',
     author_email='liriansu@gmail.com',
     url='https://github.com/LKI/md2reveal',
