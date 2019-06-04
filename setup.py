f45dd5d323434adc86865ef9266a6d261b18a858
Lirian Su <liriansu@gmail.com>
2019-06-04 15:49:55 +0800

    feat: bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='md2reveal',
-    version='1.1.5',
+    version='1.2.0',
     author='Lirian Su',
     author_email='liriansu@gmail.com',
     url='https://github.com/LKI/md2reveal',
